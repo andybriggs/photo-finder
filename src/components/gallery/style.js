285b8a4742371cd490f519e762fdde7e6ae32f13
Andy Briggs <andy.briggs@dctfh.com>
2016-10-02 19:58:02 +0100

    improved thumbnail widths

--- src/components/gallery/style.js
+++ src/components/gallery/style.js
@@ -13,10 +13,13 @@ const styles = StyleSheet.create({
   photoWrapper: {
     float: 'left',
     width: '18%',
-    marginRight: '2%',
+    marginRight: '2.5%',
     height: 100,
     overflow: 'hidden',
     marginBottom: 10,
+    ':nth-child(5n+5)': {
+      marginRight: 0
+    },
     ':hover': {
       border: '5px solid #fab630'
     }
