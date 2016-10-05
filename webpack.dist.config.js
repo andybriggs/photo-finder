7d957b56f4d949fb1428b5952ec0c6eef76685ee
Andy <andybriggs84@gmail.com>
2016-10-05 22:40:57 +0100

    added env var to dist config

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -13,6 +13,11 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: false,
       mangle: false
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+         NODE_ENV: JSON.stringify("production")
+       }
     })
   ],
   module: {
