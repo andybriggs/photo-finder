6b7ac695a763dff9e7fea0d482cb8b4701d9c86f
Andy <andybriggs84@gmail.com>
2016-10-05 07:10:24 +0100

    added build config

---
+++ webpack.dist.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css", { allChunks: true }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: false
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+    },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+    },
+    {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'file-loader'
+    }]
+  }
+};
